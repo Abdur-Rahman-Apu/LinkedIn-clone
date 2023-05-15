5b6f700d3efe6618a00edf843a5913f80d73e003
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-05-15 07:17:47 +0600

    Sidebar activity toggle is done

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const profileMenu = document.querySelector(".profile-menu-wrap");
+const sideBarActivity = document.querySelector(".sidebar-activity");
+const showMoreActivity = document.querySelector("#show-more-activity");
 
 function toggleMenu() {
   profileMenu.classList.toggle("open-menu");
+}
+
+function toggleActivity() {
+  sideBarActivity.classList.toggle("open-activity");
+  if (sideBarActivity.classList.contains("open-activity")) {
+    showMoreActivity.innerHTML = "Show less <b>-</b>";
+  } else {
+    showMoreActivity.innerHTML = "Show more <b>+</b>";
+  }
 }
